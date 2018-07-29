3bbfad8d2ec50e9f5b31cb383950d9225ca398f0
Christophe '116' Loiseau <116@lab0.net>
2018-07-29 21:04:49 +0200

    Anagram fast version

--- src/main/java/anagram/Anagram.kt
+++ src/main/java/anagram/Anagram.kt
@@ -2,42 +2,45 @@ package anagram
 
 import java.nio.file.Files
 import java.nio.file.Paths
+import java.time.Duration
+import java.time.Instant
 
 /**
  * Finds and lists english anagrams by number of letters
  */
 fun main(args: Array<String>) {
 
+    checkpoint()
     val path = "/home/ununhexium/dev/data/english/words.txt"
     val words = Files.readAllLines(Paths.get(path))
+
+    checkpoint()
     println("READ")
     words
-        .map {word ->
-            word to word.groupBy { it }.mapValues { it.value.size }
-        }
-        .also {
-            println("Mapped")
-        }
         .groupBy {
-            it.second
+            it.map { it }.sorted().joinToString(separator = "")
         }
         .also {
+            checkpoint()
             println("Grouped")
-        }
-        .mapValues {
-            it.value.map { it.first }
-        }
-        .also {
-            println("Mapped 2")
         }
         .filter {
             it.value.size > 1
         }
         .also {
+            checkpoint()
             println("Filtered")
         }
         .forEach {
             println(it.value.joinToString(separator = ", ") { it })
         }
+    checkpoint()
 }
 
+var time = Instant.now()
+
+fun checkpoint() {
+    val now = Instant.now()
+    println(Duration.between(time, now))
+    time = now
+}
