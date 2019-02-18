653114bf8a3ee11fbbaed8eaf575c8a7f34c0e9c
Christophe '116' Loiseau <116@lab0.net>
2019-02-18 18:49:51 +0100

    Check input

--- src/main/java/strassenbahn/ThriftTicket.kt
+++ src/main/java/strassenbahn/ThriftTicket.kt
@@ -4,25 +4,30 @@ interface Ticket {
     fun getPrice(area: Int): Float
     val description: String
     val adultCapacity: Int
+    val childCapacity: Int
 }
 
-data class Wanted {
-    val adults = 0;
+data class Wanted(val area: Int, val adults: Int = 0) {
+    fun noPassenger() = adults <= 0
 }
 
 class TicketImpl(
     override val description: String,
-    override val adultCapacity: Int,
-    val prices: Map<Int, Float>
+    val prices: Map<Int, Number>,
+    override val adultCapacity: Int = 0,
+    override val childCapacity: Int = 0
 ) : Ticket {
     override fun getPrice(area: Int): Float {
-        return prices[area] ?: throw IllegalArgumentException("No price for area $area")
+        return prices[area]?.toFloat() ?: throw IllegalArgumentException("No price for area $area")
     }
 }
 
 /**
  * Find the cheapest price given a request and an offer
  */
-fun cheapestPrice(adult: Wanted, offer: List<Ticket>): Float {
+fun cheapestPrice(wanted: Wanted, offer: List<Ticket>): Float {
+    if (wanted.noPassenger()) {
+        throw IllegalArgumentException("Need at least 1 traveler")
+    }
     return 0f
 }
--- src/test/java/strassenbahn/ThriftTicketKtTest.kt
+++ src/test/java/strassenbahn/ThriftTicketKtTest.kt
@@ -1,17 +1,52 @@
 package strassenbahn
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 internal class ThriftTicketKtTest {
+
+    companion object {
+        val groupTicket = TicketImpl(
+            "Group ticket",
+            mapOf(
+                1 to 10,
+                2 to 12,
+                3 to 14.5
+            ),
+            5,
+            0
+        )
+
+        val oneWayAdult = TicketImpl(
+            "Adult one way",
+            mapOf(
+                1 to 3.00,
+                2 to 3.50,
+                3 to 4.80,
+                4 to 5.50
+            )
+        )
+    }
 
 
     @Test
     fun `throws an exception when there is noone on the ride`() {
-
+        assertThat(
+            assertThrows<IllegalArgumentException> {
+                cheapestPrice(Wanted(1), listOf(oneWayAdult))
+            }.message
+        ).isEqualTo(
+            "Need at least 1 traveler"
+        )
     }
 
     @Test
-    fun `can find the cheapest ticket when there is only 1 person`() {
-
+    fun `can find the cheapest ticket when there is only 1 person and 1 offer`() {
+        assertThat(
+            cheapestPrice(Wanted(1, adults = 1), listOf(oneWayAdult))
+        ).isEqualTo(
+            5.50
+        )
     }
 }
